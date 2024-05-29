2eb5679460847b3e095426ae54d40058eb9773bc
CHI-342 <59065261+Chi342@users.noreply.github.com>
2024-05-29 11:31:33 -0300

    Exportar a excel

--- Clases/InterfazExcel.py
+++ Clases/InterfazExcel.py
@@ -10,5 +10,26 @@ class InterfazExcel:
         pass
 
     def exportarExcel(self, vinos):
-        pass
+        workbook = xlsxwriter.Workbook('RankingVinos.xlsx')
+        worksheet = workbook.add_worksheet()
+        letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+        for i in range(len(vinos)):
+            for j in range((len(vinos[i]))):
+                casilla = letras[j] + str(i)
+                worksheet.write(casilla, vinos[i][j])
+        workbook.close()
 
+
+vino1 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino2 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino3 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino4 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino5 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino6 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino7 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino8 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino9 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+vino10 = ['Balbo', 1500, 'Sauvignon', 'Bon Vino', 'Mendoza', 'Argentina', 100]
+Vinos = [vino1, vino2, vino3, vino4, vino5, vino6, vino7, vino8, vino9, vino10]
+intExcel = InterfazExcel()
+intExcel.exportarExcel(Vinos)
