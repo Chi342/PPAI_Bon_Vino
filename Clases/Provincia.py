222793d000afb11deb23f3f47f640b511271f20d
robertoutn <57816650+robertoutn@users.noreply.github.com>
2024-05-30 21:18:54 -0300

    Refactor code for initializing classes with parameters

--- Clases/Provincia.py
+++ Clases/Provincia.py
@@ -2,16 +2,16 @@
 #-*- coding: utf-8 -*-
 
 class Provincia:
-    def __init__(self):
-        self.nombre = None
-        self.pais = None
+    def __init__(self, nombre, pais):
+        self.nombre = nombre
+        self.pais = pais
 
     def obtenerPais(self, ):
         return self.pais.getNombre()
 
     def getNombre(self, ):
-        pass
+        return self.nombre
 
     def setNombre(self, value):
-        pass
+        self.nombre = value
 
